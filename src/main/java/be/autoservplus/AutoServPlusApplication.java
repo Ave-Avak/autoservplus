55ebad90d0ecf9de44de9b91d539313c32a7a9c8
Avak Avedisian <avak@hotmail.be>
2026-08-19 17:59:32 +0200

    feat(config): ajouter la classe principale de l application

---
+++ src/main/java/be/autoservplus/AutoServPlusApplication.java
@@ -0,0 +1,20 @@
+﻿package be.autoservplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Point d entree de la plateforme AutoServ+.
+ *
+ * <p>L application suit une architecture de monolithe modulaire : chaque package de
+ * premier niveau correspond a un domaine metier identifie dans le rapport d analyse
+ * UML (livrable 07), et les dependances entre modules passent exclusivement par la
+ * couche service.</p>
+ */
+@SpringBootApplication
+public class AutoServPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoServPlusApplication.class, args);
+    }
+}
